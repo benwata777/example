8ce280eb68ac1f3bde84f66580d0eb46f09af7b4
MoominniM <moominnim@MoominniMs-M3-Pro.local>
2024-12-26 17:58:47 +0700

    Filmmm header

--- client/my-nuxt-app/pages/film.vue
+++ client/my-nuxt-app/pages/film.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-      <h1>Film</h1>
+      <h1>Filmmmmmmm</h1>
       <nuxt-link to="/">Go to Home</nuxt-link>
     </div>
   </template>
-  
+
   <script lang="ts">
   export default {
     name: 'FilmPage',
   }
   </script>
-  
